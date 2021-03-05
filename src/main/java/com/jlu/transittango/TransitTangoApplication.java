a0e6c86ddf68bc340c87ec5a01b09d9b9b935bcc
Joshua Luppes <cooljoebob64@gmail.com>
2021-03-05 08:19:08 -0600

    Create initial project files from Spring Initializer

---
+++ src/main/java/com/jlu/transittango/TransitTangoApplication.java
@@ -0,0 +1,13 @@
+package com.jlu.transittango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitTangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitTangoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jlu/transittango/TransitTangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlu.transittango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitTangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
