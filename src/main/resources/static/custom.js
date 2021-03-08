cadfd27aaa1dc33d35c361f871bb0891c42e116a
Joshua Luppes <cooljoebob64@gmail.com>
2021-03-08 16:52:29 -0600

    Update formatting for HTML, CSS, and JS files

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,19 +1,25 @@
 var map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: parseFloat(busLocations[0].LATITUDE), lng: parseFloat(busLocations[0].LONGITUDE) },
-        zoom: 15,
-        scrollwheel: false
-    });
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: parseFloat(busLocations[0].LATITUDE),
+      lng: parseFloat(busLocations[0].LONGITUDE),
+    },
+    zoom: 15,
+    scrollwheel: false,
+  });
 
-    for (i=0; i<busLocations.length; i++){
-        var marker = new google.maps.Marker({
-            position: { lat: parseFloat(busLocations[i].LATITUDE), lng: parseFloat(busLocations[i].LONGITUDE) },
-            map: map,
-            icon:{
-                url:"/bus.png",
-                scaledSize: new google.maps.Size(25,25)
-            }
-        });
-    }
+  for (i = 0; i < busLocations.length; i++) {
+    var marker = new google.maps.Marker({
+      position: {
+        lat: parseFloat(busLocations[i].LATITUDE),
+        lng: parseFloat(busLocations[i].LONGITUDE),
+      },
+      map: map,
+      icon: {
+        url: "/bus.png",
+        scaledSize: new google.maps.Size(25, 25),
+      },
+    });
+  }
 }
